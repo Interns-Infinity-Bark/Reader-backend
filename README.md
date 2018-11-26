1c9013894ccb37dbbe70d7e0b200a3079fb2a437
QAQrz <1366138143@qq.com>
2018-11-26 23:30:32 +0800

    Add cors

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Votee - 投票票
 
 #### Usage
 
-- `git clone https://github.com/QAQrz/Votee.git`
+- `git clone https://github.com/Interns-Infinity-Bark/Votee.git`
 - Copy `src/config_default.ts` to `src/config.ts` and edit it
 - `npm install`
 - `npm run build`
--- src/routers/admin.ts
+++ src/routers/admin.ts
@@ -1,5 +1,5 @@
 import * as Router from 'koa-router';
-import { admin, login, logout, modifyNickname, modifyPassword, userlist } from '../controllers/admin';
+import { admin, login, logout, modifyNickname, modifyPassword, users } from '../controllers/admin';
 import { requireAdmin } from '../controllers';
 
 export const router = new Router()
@@ -8,4 +8,4 @@ export const router = new Router()
     .get('/', requireAdmin, admin)
     .put('/nickname', requireAdmin, modifyNickname)
     .put('/password', requireAdmin, modifyPassword)
-    .get('/userlist', requireAdmin, userlist);
+    .get('/users', requireAdmin, users);
